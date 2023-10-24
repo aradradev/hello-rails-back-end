3690ed009c99dbc38c89b3bd1727899ac2d60bfc
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-24 09:45:03 +0000

    fixing linters issue

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::GreetingsController < ApplicationController
-    def random_greeting
-        @greeting = Greeting.order('RANDOM()').first
-        render json: {greeting: @greeting.text}
-    end
+  def random_greeting
+    @greeting = Greeting.order('RANDOM()').first
+    render json: { greeting: @greeting.text }
+  end
 end
