e6d8fbd377b0638c1a0036c81eeb7d5d06bd4090
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-24 09:20:01 +0000

    adding config in app rb

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module HelloRailsBackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.insert_before 0, Rack::Corse do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://127.0.0.1:3000'
         resource '*',
