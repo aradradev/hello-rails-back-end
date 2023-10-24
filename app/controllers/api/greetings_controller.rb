eba9955b7db814efd390f81e5b6c27bf6d749016
Abdoul <abdoulramanediallo44@gmail.com>
2023-10-24 09:24:32 +0000

    implement api logic

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,2 +1,6 @@
 class Api::GreetingsController < ApplicationController
+    def random_greeting
+        @greeting = Greeting.order('RANDOM()').first
+        render json: {greeting: @greeting}
+    end
 end
